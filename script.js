f36c331d2557b5eb55b64a8ef09aaae1c717e612
Al3ncar <alencar29htcss@gmail.com>
2022-01-20 06:55:42 -0300

    Desafio 04

---
+++ script.js
@@ -0,0 +1,38 @@
+//---------------01------------------//
+
+let text1 = " Loop do 1 até 10 usando For"
+console.log(text1)
+
+for(let x = 1; x <= 10; x++)
+console.log(x)
+
+//--------------02------------------//
+
+let text2 = "Loop do 1 até 10 usando While"
+console.log(text2)
+
+let number = 1
+
+while(number <= 10){
+
+    console.log(number)
+    number ++
+}
+
+//--------------03------------------//
+
+let text3 = "Loop do 1 até 100 apenas os ímpares"
+console.log(text3)
+
+for(let z = 1; z <= 100; z +=2){
+    console.log(z)
+}
+
+//--------------04-----------------//
+
+let text4 = "Loop do 0 a 100 usando os pares"
+console.log(text4)
+
+for(let y = 0; y <= 100; y +=2){
+    console.log(y)
+}
